8f3ba45ebe38665ab7b28f590eb9d235e321caf4
Pablo <pablomarques.trabalho@outlook.com>
2024-03-17 17:08:59 -0300

    Inicio do projeto!

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+/* 	
+    Projeto do Cruzamento Automático - versão 1!
+	Simula carros em um cruzamento simples.
+*/
+
+
+/* 
+	
+	Geometria do cruzamento
+
+						largura V
+				margem V|    |
+						|    |
+						|    |margem H
+------------------------+----+--------
+	ViaH  > > > 	   	|    |	    largura H
+------------------------+----+--------
+	perímetro H			|    |
+						|    |
+						|    |
+						| ^  |perímetro V
+						| ^  |
+						| ^  |
+						|ViaV|
+						|    |
+
+*/
+
+
+fn main() {
+	println!("Inicio do programa");
+	println!("Fim da simulação");
+}
